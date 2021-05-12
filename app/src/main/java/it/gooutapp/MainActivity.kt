e5afe0b204bfa7caa7ae76fe96342c8abf431a4c
Luca Lattarulo <llattarulo@studenti.uninsubria.it>
2021-05-12 18:50:06 +0200

    aggiunto bottone prova

--- app/src/main/java/it/gooutapp/MainActivity.kt
+++ app/src/main/java/it/gooutapp/MainActivity.kt
@@ -7,7 +7,7 @@ import android.view.View
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login)
     }
 
     fun checkLogin(view: View) {
