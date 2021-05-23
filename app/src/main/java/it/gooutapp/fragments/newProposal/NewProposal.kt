a95ecff82f7195ffd7b5f3bf140a0eab71727a8f
lucalat <llattarulo@studenti.uninsubria.it>
2021-05-23 14:26:19 +0200

    aggiunto layout a fragment_new_proposal.xml con view CalendarView

--- app/src/main/java/it/gooutapp/fragments/newProposal/NewProposal.kt
+++ app/src/main/java/it/gooutapp/fragments/newProposal/NewProposal.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import it.gooutapp.R
 
 class NewProposal : Fragment() {
-
+    lateinit var calendar: CalendarView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_new_proposal, container, false)
+        calendar = root.findViewById(R.id.calendarView)
+
+        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
+            val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
+            Toast.makeText(calendar.context, msg, Toast.LENGTH_SHORT).show()
+        }
 
         return root
+    }
+
+
+
+    fun confirmDate(view: View){
+
     }
 }
