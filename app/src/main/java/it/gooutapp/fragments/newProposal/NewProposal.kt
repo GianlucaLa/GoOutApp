02af8a19f6c76cbaec45b6873c29da69f5befd2d
Luca Lattarulo <llattarulo@studenti.uninsubria.it>
2021-05-24 18:00:48 +0200

    Sistemato problema LoginActivity.kt e RegistrationActivity.kt

--- app/src/main/java/it/gooutapp/fragments/newProposal/NewProposal.kt
+++ app/src/main/java/it/gooutapp/fragments/newProposal/NewProposal.kt
@@ -13,13 +13,13 @@ class NewProposal : Fragment() {
     lateinit var calendar: CalendarView
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_new_proposal, container, false)
-        calendar = root.findViewById(R.id.calendarView)
+        /*calendar = root.findViewById(R.id.calendarView)
 
         calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
             // Note that months are indexed from 0. So, 0 means January, 1 means february, 2 means march etc.
             val msg = "Selected date is " + dayOfMonth + "/" + (month + 1) + "/" + year
             Toast.makeText(calendar.context, msg, Toast.LENGTH_SHORT).show()
-        }
+        }*/
 
         return root
     }
