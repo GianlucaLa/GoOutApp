30e43d721dd9636b9a530242ab93f0b949574612
kuka6 <glatronico@studenti.uninsubria.it>
2021-06-04 17:23:07 +0200

    funzionalità swipe a destra per recycleview in modo che vengano cancellate

--- app/src/main/java/it/gooutapp/fragments/showGroup/MyAdapter.kt
+++ app/src/main/java/it/gooutapp/fragments/showGroup/MyAdapter.kt
@@ -9,7 +9,7 @@ import it.gooutapp.R
 import it.gooutapp.models.Group
 
 
-class MyAdapter(private val groupList : ArrayList<Group>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+class MyAdapter(private val groupList : ArrayList<Group>, val clickListener: ClickListener) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyViewHolder {
 
@@ -20,26 +20,30 @@ class MyAdapter(private val groupList : ArrayList<Group>) : RecyclerView.Adapter
 
     override fun onBindViewHolder(holder: MyAdapter.MyViewHolder, position: Int) {
 
-
         val group : Group = groupList[position]
         holder.groupName.text = group.groupName
         val iniziale = group.groupName?.get(0)
         holder.groupPosition.text = "${position+1}"
         //icona con iniziale
         holder.icon.text = iniziale.toString().toUpperCase()
+        holder.itemView.setOnClickListener{
+            clickListener.onItemClick(groupList[position])
+        }
 
     }
 
     override fun getItemCount(): Int {
-
        return groupList.size
     }
 
-    public class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //nome della textView dove inserire dato
         val groupName : TextView = itemView.findViewById(R.id.textViewNomeGruppo)
         val groupPosition : TextView = itemView.findViewById(R.id.textViewNumGruppo)
         val icon : TextView = itemView.findViewById(R.id.textViewDrawable)
+    }
+
+    interface ClickListener {
+        fun onItemClick(group: Group)
     }
 }
--- app/src/main/java/it/gooutapp/fragments/showGroup/ShowGroupFragment.kt
+++ app/src/main/java/it/gooutapp/fragments/showGroup/ShowGroupFragment.kt
@@ -1,5 +1,6 @@
 package it.gooutapp.fragments.showGroup
 
+import android.graphics.Canvas
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -12,6 +13,7 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -22,7 +24,7 @@ import it.gooutapp.firebase.FireStore
 import it.gooutapp.models.Group
 import kotlin.random.Random
 
-class ShowGroupFragment : Fragment() {
+class ShowGroupFragment : Fragment(), MyAdapter.ClickListener {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var groupArrayList: ArrayList<Group>
@@ -30,7 +32,6 @@ class ShowGroupFragment : Fragment() {
     private var user_email = Firebase.auth.currentUser?.email.toString()
     private val fs = FireStore()
     private val TAG = "SHOW_GROUP_FRAGMENT"
-    private val groupId = "grpdoc1"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_show_group, container, false)
@@ -42,14 +43,26 @@ class ShowGroupFragment : Fragment() {
         recyclerView.setHasFixedSize(true)
         groupArrayList = arrayListOf()
 
-        fs.getUserGroupData(user_email){ groupList ->
+        fs.getUserGroupData(user_email) { groupList ->
             Log.w(TAG, groupList.toString())
             groupArrayList = groupList
             Log.w(TAG, groupArrayList.toString())
-            myAdapter = MyAdapter(groupArrayList)
+            myAdapter = MyAdapter(groupArrayList, this)
             recyclerView.adapter = myAdapter
             myAdapter.notifyDataSetChanged()
 
+            //swipe a destra recycle view
+            val itemSwipe = object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN,  ItemTouchHelper.LEFT) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+               return false
+            }
+            //TODO cercare come cambiare background durante swipe
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                showDialog(viewHolder)
+            }
+        }
+            val swap = ItemTouchHelper(itemSwipe)
+            swap.attachToRecyclerView(recyclerView)
         }
 
         //Create Group Listener
@@ -66,10 +79,15 @@ class ShowGroupFragment : Fragment() {
                     var nomeG = editText.text.toString()
                     if (nomeG != "") {
                         fs.createGroupData(nomeG, user_email) { result ->
-                            Toast.makeText(root.context, R.string.group_creation, Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                root.context,
+                                R.string.group_creation,
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
-                    }else{
-                        Toast.makeText(root.context, R.string.error_empty_value, Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(root.context, R.string.error_empty_value, Toast.LENGTH_SHORT)
+                            .show()
                     }
                 }
                 setNegativeButton(R.string.cancel) { dialog, which ->
@@ -81,4 +99,28 @@ class ShowGroupFragment : Fragment() {
         }
         return root
     }
+
+    //popup per confermare o cancellare row
+    private fun showDialog(viewHolder: RecyclerView.ViewHolder) {
+        val builder = view?.let { AlertDialog.Builder(it.context) }
+        if (builder != null) {
+            builder.setTitle(R.string.delete_row)
+            builder.setMessage(R.string.delete_row_message)
+            builder.setPositiveButton(R.string.ok) {dialog, wich ->
+                val position = viewHolder.adapterPosition
+                groupArrayList.removeAt(position)
+                myAdapter.notifyItemRemoved(position)
+            }
+            builder.setNegativeButton(R.string.cancel){dialog, wich ->
+                val position = viewHolder.adapterPosition
+                myAdapter.notifyItemChanged(position)
+            }
+            builder.show()
+        }
+    }
+
+    //TODO per creare nuovo layout
+//       override fun onItemClick(group: Group) {
+//        TODO("Not yet implemented")
+//    }
 }
