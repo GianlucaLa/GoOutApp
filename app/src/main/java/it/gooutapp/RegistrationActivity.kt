d273b0cae34259bfc9158a927cf69bace66712ba
Luca Lattarulo <llattarulo@studenti.uninsubria.it>
2021-06-16 20:36:12 +0200

    nuove editetext in login e registration

--- app/src/main/java/it/gooutapp/RegistrationActivity.kt
+++ app/src/main/java/it/gooutapp/RegistrationActivity.kt
@@ -4,15 +4,17 @@ package it.gooutapp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import android.util.Patterns
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import it.gooutapp.firebase.FireStore
+import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.registration.*
 
 
@@ -23,6 +25,13 @@ class RegistrationActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.registration)
+        editTextPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                editTextPasswordView.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
 
     fun closeActivity() {
@@ -54,40 +63,52 @@ class RegistrationActivity: AppCompatActivity() {
         var isNicknameValid = false
         val name = editTextName.text.toString()
         val surname = editTextSurname.text.toString()
-        val nickanme = editTextNickname.text.toString()
+        val nickname = editTextNickname.text.toString()
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
 
         // Check for a valid email address.
-        if (email.isEmpty()) {
-            editTextEmail.setError(getString(R.string.email_error))
-            isEmailValid = false
-        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            editTextEmail.setError(resources.getString(R.string.error_invalid_email))
-            isEmailValid = false
-        } else {
-            isEmailValid = true
+        when {
+            email!!.isEmpty() -> {
+                editTextEmailView.endIconMode = TextInputLayout.END_ICON_CLEAR_TEXT
+                editTextEmail.error = resources.getString(R.string.email_error)
+                isEmailValid = false
+            }
+            email.length < 6 -> {
+                editTextEmail.error = resources.getString(R.string.error_invalid_email)
+                isEmailValid = false
+            }
+            else -> {
+                isEmailValid = true
+            }
         }
 
+
         // Check for a valid password.
-        if (password.isEmpty()) {
-            editTextPassword.setError(resources.getString(R.string.password_error))
-            isPasswordValid = false
-        } else if (password.length < 6) {
-            editTextPassword.setError(resources.getString(R.string.error_invalid_password))
-            isPasswordValid = false
-        } else {
-            isPasswordValid = true
+        when {
+            password!!.isEmpty() -> {
+                editTextPasswordView.endIconMode = TextInputLayout.END_ICON_NONE
+                editTextPassword.error = resources.getString(R.string.password_error)
+                isPasswordValid = false
+            }
+            password.length < 6 -> {
+                editTextPasswordView.endIconMode = TextInputLayout.END_ICON_NONE
+                editTextPassword.error = resources.getString(R.string.error_invalid_password)
+                isPasswordValid = false
+            }
+            else -> {
+                isPasswordValid = true
+            }
         }
 
         //check others
         isNameValid = !name.isEmpty()
         isSurnameValid = !surname.isEmpty()
-        isNicknameValid = !nickanme.isEmpty()
+        isNicknameValid = !nickname.isEmpty()
 
         //final check
         if (isEmailValid && isPasswordValid && isNameValid && isSurnameValid && isNicknameValid) {
-            fs.createUserData(name, surname, nickanme, email)
+            fs.createUserData(name, surname, nickname, email)
             createUser(email, password)
         }else{
             Toast.makeText(applicationContext, getString(R.string.incorrect_fields_registration), Toast.LENGTH_SHORT).show()
